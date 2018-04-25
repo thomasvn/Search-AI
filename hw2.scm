142b4f48ee92c5d49a0aed699caa57b209e5dc84
thomasvn <thomasnguyen96@gmail.com>
2018-04-25 14:01:26 -0700

    In the middle of commenting ... about to change the format-swapped function

--- hw2.scm
+++ hw2.scm
@@ -107,32 +107,41 @@
 
 
 ; This function formats the output of the swapped elements
+; INPUT: (format-swapped ((Nevada Utah California) (California Nevada Utah) (Utah California Nevada)) ((1 3) (2 3) (1 2)) ())
+; OUTPUT: (((Nevada Utah California) ((1 3))) ((California Nevada Utah) ((2 3))) ((Utah California Nevada) ((1 2))))
 (define (format-swapped elements swaps prev)
     (cond
-        ; Base Case: If we've gone through the entire list of swaps, return empty list
-        ((null? swaps) '())
+        (
+            ; Base Case: If we've gone through the entire list of swaps, return empty list
+            (null? swaps) 
+                '()
+        )
         ; Pair the element with the swap that was made, then recursively continue
-        (#t (cond 
+        (#t 
+            (cond 
                 (
                     (null? prev) 
                         (cons (cons (car elements) (list (cons (car swaps) prev))) (format-swapped (cdr elements) (cdr swaps) prev))
                 )
-
                 (
                     #t
-                        (cons (cons (car elements)  (list(cons (car swaps) prev ))) (format-swapped (cdr elements) (cdr swaps) prev))
+                        (cons (cons (car elements) (list(cons (car swaps) prev))) (format-swapped (cdr elements) (cdr swaps) prev))
                 )
             )
         )
     )
 )
 
+(display (format-swapped '((Nevada Utah California) (California Nevada Utah) (Utah California Nevada)) '((1 3) (2 3) (1 2)) '()))
+
+
 ; This function will return all children of the current state by returning all
 ; possible swaps that can be made.
 ; INPUT: (get-children ‘((California Nevada) ()))
 ; OUTPUT: (((Nevada California) (1 2)))
 (define (get-children node)
-    (let ((swaps (all-swaps 1 (length (nth-item 1 node))))
+    (let 
+        ((swaps (all-swaps 1 (length (nth-item 1 node))))
         (prev-states (nth-item 2 node)))
         ; Swap all the states, then format the output
         (format-swapped (swap-all (nth-item 1 node) swaps) swaps prev-states)
@@ -145,20 +154,37 @@
 ; OUTPUT: #t or #f
 (define (is-goal-state? node)
     ; state variable has list of swapped locations
-    (let ((state (car node))
+    (let 
+        ((state (car node))
         ; next-state variable is just the tail of the state variable formatted with the original input
         (next-state (cons (cdr (nth-item 1 node)) (list (nth-item 2 node)))))
-        ; If there's only a single item in the list, it automatically passes
-        (cond ((null? (cdr state)) #t)
-            ; Check whether the first two items are adjacent
-            ((is-adjacent? (nth-item 1 state) (nth-item 2 state) adjacency-map)
-                ; When we're down to our last two states we are finished, else recurse through states
-                (cond ((= (length state) 2) #t)
-                    (#t (is-goal-state? next-state))
-                )
+        (cond 
+            (
+                ; If there's only a single item in the list, technically it is a goal state
+                (null? (cdr state)) 
+                    #t
             )
-            ; If is-adjacent? did not return true for given two items, return false
-            (#t #f)
+            (
+                ; Check whether the first two items are adjacent
+                (is-adjacent? (nth-item 1 state) (nth-item 2 state) adjacency-map)
+                    (cond 
+                        (
+                            ; When only two states left, we have finished checking all states
+                            (= (length state) 2) 
+                                #t
+                        )
+                        (
+                            ; Else, keep checking whether the next two states are adjacent
+                            #t
+                                (is-goal-state? next-state)
+                        )
+                    )
+            )
+            (
+                ; If is-adjacent? did not return true for given two items, return false
+                #t
+                    #f
+            )
         )
     )
 )
@@ -166,6 +192,8 @@
 
 ; This function will format the starting point of the frontier to include 
 ; internal state representation
+; INPUT: (format-frontier '(California Utah Nevada))
+; OUTPUT: ((California Utah Nevada) ())
 (define (format-frontier frontier)
     (list (cons frontier (list '())))
 )
@@ -173,23 +201,24 @@
 
 ; This function takes a frontier, and iterates through all its nodes to check if
 ; one of the nodes is the goal-state
+; INPUT: (((California Nevada Utah) ((2 3))) ((Utah California Nevada) ((1 2))) ((California Utah Nevada) ()))
+; OUTPUT: #t or #f
 (define (frontier-has-goal? frontier)
-    (cond ((is-goal-state? (car frontier)) #t)
-        ((null? (cdr frontier)) #f)  ; Base Case: No more items to check
-        (#t (frontier-has-goal? (cdr frontier)))
-    )
-)
-
-
-; This function will do a depth first search on the frontier
-(define (dfs frontier)
-    ; The next-state variable is the frontier without the currently visited state and corresponding swap history
-    (let ((next-state (cond (null? (nth-item 2 frontier)) (append (cdr (nth-item 1 frontier)) (car (nth-item 2 frontier)))
-            (#t (append (cdr (nth-item 1 frontier)) (cdr (nth-item 2 frontier)))))))
-        (cond
-            ((null? (car frontier)) #f)  ; Base Case: if you have a null list, you failed
-            ((frontier-has-goal? frontier) frontier)  ; Check the the frontier if it's the goal state
-            (#t (dfs (append (get-children frontier) next-state)))  ; Append children of frontier with the cdr of the frontier back into dfs
+    (cond 
+        (
+            ; Check if the state of the head of the list is a goal state
+            (is-goal-state? (car frontier)) 
+                #t
+        )
+        (
+            ; Base Case: No more items to check
+            (null? (cdr frontier)) 
+                #f
+        )
+        (
+            ; Recurse down the list to check through all states
+            #t
+                (frontier-has-goal? (cdr frontier))
         )
     )
 )
@@ -199,6 +228,7 @@
 ; INPUT: (i-dfs '((California Utah Nevada) ()) 2)
 ; OUTPUT: #f or ((Utah Nevada California) ((1 2) (1 3)))
 (define (i-dfs frontier max-depth)
+    (begin (display frontier) (newline) (newline)
     (cond
         (
             ; Base Case: If the frontier is empty, do not search
@@ -206,15 +236,22 @@
                 #f
         )
         (
-            ; Base Case: If frontier only has one element, it works
+            ; Base Case: If frontier only has one element, no need to search
             (equal? (length (car (car frontier))) 1)
                 #t
         )
         (#t
-            (let ((next-state (cond ((null? (cdr frontier)) frontier)
-                    (#t (cdr frontier)))))
+            (let
+                ; The next-state variable describes the machine's state at the next node
+                ((next-state 
+                    (cond
+                        ((null? (cdr frontier)) frontier)
+                            (#t (cdr frontier))
+                    )
+                ))
                 (cond
                     (
+                        ; Return solution state if we've reached the goal state
                         (is-goal-state? (car frontier))
                             (car frontier)
                     )
@@ -229,6 +266,7 @@
                             (i-dfs next-state max-depth)
                     )
                     (
+                        ; Recurse down the tree by generating children
                         #t 
                             (i-dfs (append (get-children (car frontier)) next-state) max-depth)
                     )
@@ -236,10 +274,11 @@
             )
         )
     )
+    )
 )
 
 
-; Helper function for the id-dfs
+; Helper function for the id-dfs which maintains depth as an argument
 ; INPUT: (id-dfs-helper '(California Utah Nevada) 0 2)
 ; OUTPUT: #f or ((Utah Nevada California) ((1 2) (1 3)))
 (define (id-dfs-helper locations curr-depth max-depth)
@@ -250,21 +289,22 @@
                 #f
         )
         (
-            ; Base Case: If one location, return true
+            ; Base Case: If one location, return result
             (equal? (length locations) 1)
                 (car (format-frontier locations))
         )
         (
-            ; Base Case: If we have tried all depths to the max depth, return false
+            ; Base Case: If we have tried all depths up to the max depth, return false
             (equal? curr-depth max-depth)
                 #f
         )
         (
+            ; If there is a dfs solution with this current depth, return solution
             (list? (i-dfs (format-frontier locations) curr-depth))
                 (i-dfs (format-frontier locations) curr-depth)
         )
         (
-            ; If the DFS with the previous depth failed, go here
+            ; If the DFS with the previous depth failed, try next depth
             #t
                 (id-dfs-helper locations (+ curr-depth 1) max-depth)
         )
@@ -281,6 +321,9 @@
 ; of locations sorted into a valid solution state. The second return item will 
 ; be a list of pairs indicating which items need to be swapped in order to reach 
 ; the solution state.
+; INPUT: (id-dfs '(Tennessee Iowa Kentucky North-Carolina Missouri))
+; OUTPUT: ((North-Carolina Tennessee Kentucky Missouri Iowa) ((1 4) (2 5) (1 5)))
+; ASSUMES: The list of locations does not have sublists, and has valid entries
 (define (id-dfs locations)
     (id-dfs-helper locations 1 (length locations))
 )
